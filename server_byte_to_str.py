c75e00bfe93a735bf0ba9f46a85756acae7d9ddb
Yuji Nishida <yuji.nishida@example.com>
2018-10-14 15:57:38 +0900

    This is to open the file as str mode and convert byte to str when it is written to the file.

---
+++ server_byte_to_str.py
@@ -0,0 +1,20 @@
+import tornado.ioloop
+import tornado.web
+
+class PostHandler(tornado.web.RequestHandler):
+    def post(self):
+        program_content = self.request.files["files_index"][0]["body"]
+        filename = self.request.files["files_index"][0]["filename"]
+        print(program_content)
+        print(filename)
+        filename = "server_directory/" + filename
+        with open(filename,"w") as f:
+            f.write(program_content.decode())
+ 
+app = tornado.web.Application([
+        (r"/post/", PostHandler)
+    ])
+
+app.listen(8888)
+tornado.ioloop.IOLoop.current().start()
+
